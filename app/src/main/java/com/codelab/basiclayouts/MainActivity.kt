13c11c1eafc4048ace4d0558bbc4619aec37b2e8
Jack Sage-Phoenix <jack@jackmeyers.co>
2024-05-15 14:44:29 -0500

    9. Home section - Slot APIs

--- app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -197,9 +197,20 @@ fun FavoriteCollectionsGrid(
 // Step: Home section - Slot APIs
 @Composable
 fun HomeSection(
-    modifier: Modifier = Modifier
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
 ) {
-    // @TODO: Implement composable here
+    Column(modifier) {
+        Text(
+            text = stringResource(title),
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp),
+            style = MaterialTheme.typography.titleMedium,
+        )
+        content()
+    }
 }
 
 // Step: Home screen - Scrolling
@@ -306,7 +317,11 @@ fun AlignYourBodyRowPreview() {
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun HomeSectionPreview() {
-    MySootheTheme { HomeSection() }
+    MySootheTheme {
+        HomeSection(R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
